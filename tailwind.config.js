bbba64413012c5edf075d20825a4601c0343c93f
JulieZold <juliezold7890j@gmail.com>
2025-02-26 18:58:28 +0700

    feat(index): เพิ่มโครงสร้างเว็บ

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#CC0D22',   /* สีแดงเข้ม */
+                secondary: '#ED2B3D', /* สีแดงกลาง */
+                light: '#FFD8D6',     /* สีชมพูอ่อน */
+                accent: '#FFB7C6',    /* สีชมพูเข้ม */
+                darkblue: '#000181'   /* สีน้ำเงินเข้ม */
+            }
+        }
+    }
+};
